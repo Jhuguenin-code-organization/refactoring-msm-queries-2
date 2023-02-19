9b0dccbec2ee3b742e4ff858f5f388bd29a84344
Jhuguenin-code <jhuguenin@uchicago.edu>
2023-02-19 14:48:18 +0000

    All rails tests work (separate association accessor for getting actors from movies not working)

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -13,13 +13,32 @@
 #  director_id :integer
 #
 class Movie < ApplicationRecord
-  def director
-    key = self.director_id
+  belongs_to(:director, { :class_name => "Director", :foreign_key => "director_id" })
+has_many(:characters, { :class_name => "Character", :foreign_key => "movie_id" }) 
 
-    matching_set = Director.where({ :id => key })
 
-    the_one = matching_set.at(0)
+  #def director
+    #key = self.director_id
 
-    return the_one
+    #matching_set = Director.where({ :id => key })
+
+    #the_one = matching_set.at(0)
+
+    #return the_one
+  #end
+#actors associated to a movie (many-to-many)
+  def cast
+    the_many = Array.new
+
+    self.characters.each do |joining_record|
+      destination_record = joining_record.actor
+
+      actor_name = destination_record.name
+
+      the_many.push(actor_name)
+    end
+
+    return the_many
   end
+
 end
